ddf0b5cec91fecb4e92c0c3e1f753f0b09043a9a
Mariia Shytse <aleatoricmbnt@gmail.com>
2026-02-04 18:45:15 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
 resource "null_resource" "input_trigger" {
-  triggers = {
-    trigger = var.long_var
+#  triggers = {
+#    trigger = var.long_var
+#  }
+}
+
+resource "terraform_data" "approx_15MB" {
+  input = {
+    project_name = "test"
+    environment = "dev"
+    timestamp = timestamp()
   }
 }
